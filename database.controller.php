69de55570c5893047d8236b6d8253ea5f90ca5a4
AnnaJustynaK <87768750+AnnaJustynaK@users.noreply.github.com>
2023-01-18 10:33:18 +0100

    Add files via upload

---
+++ database.controller.php
@@ -0,0 +1,20 @@
+<?php
+$db_server='localhost';
+$db_user='shop_user';
+$db_password='';
+$db_name='c_online';
+$db=mysqli_connect($db_server,$db_user,$db_password,$db_name);
+
+function begin(){
+    mysqli_query($GLOBALS['db'],  "BEGIN");
+}
+
+function commit(){
+    mysqli_query($GLOBALS['db'], "COMMIT");
+}
+
+function rollback(){
+    mysqli_query($GLOBALS['db'], "ROLLBACK");
+}
+
+?>
